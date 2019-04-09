02da0925d6f7d2cedd83851dd22c6e4a355c74b5
archieLa <arturm.lach@gmail.com>
2019-04-08 21:05:51 -0500

    Organized files into folders and modified startup script

--- launch_drows_detector.sh
+++ launch_drows_detector.sh
@@ -2,7 +2,7 @@
 # Go to drows detection folder
 cd ~/Documents/Sleep_Detection/DrowsinessDetection
 # Launch the program
-python SleepDetector.py --cascade haarcascade_frontalface_default.xml --shape-predictor shape_predictor_68_face_landmarks.dat --warning-sound warning.wav > log.txt &
+python Source/SleepDetector.py --cascade RequiredFiles/haarcascade_frontalface_default.xml --shape-predictor RequiredFiles/shape_predictor_68_face_landmarks.dat --warning-sound RequiredFiles/warning.wav > log.txt &
 
 
 
